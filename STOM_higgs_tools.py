5e2de4d211b8edb37b479e7ce971efbc4aefdba8
Dillen <dillenmylee@gmail.com>
2021-06-07 11:14:32 +0100

    edit

--- STOM_higgs_tools.py
+++ STOM_higgs_tools.py
@@ -6,13 +6,13 @@ N_b = 10e5 # Number of background events, used in generation and in fit.
 b_tau = 30. # Spoiler.
 
 def generate_data(n_signals = 400):
-    ''' 
-    Generate a set of values for signal and background. Input arguement sets 
-    the number of signal events, and can be varied (default to higgs-like at 
-    announcement). 
-    
-    The background amplitude is fixed to 9e5 events, and is modelled as an exponential, 
-    hard coded width. The signal is modelled as a gaussian on top (again, hard 
+    '''
+    Generate a set of values for signal and background. Input arguement sets
+    the number of signal events, and can be varied (default to higgs-like at
+    announcement).
+
+    The background amplitude is fixed to 9e5 events, and is modelled as an exponential,
+    hard coded width. The signal is modelled as a gaussian on top (again, hard
     coded width and mu).
     '''
     vals = []
@@ -22,24 +22,24 @@ def generate_data(n_signals = 400):
 
 
 def generate_signal(N, mu, sig):
-    ''' 
+    '''
     Generate N values according to a gaussian distribution.
     '''
     return np.random.normal(loc = mu, scale = sig, size = N).tolist()
 
 
 def generate_background(N, tau):
-    ''' 
+    '''
     Generate N values according to an exp distribution.
     '''
     return np.random.exponential(scale = tau, size = int(N)).tolist()
 
 
 def get_B_chi(vals, mass_range, nbins, A, lamb):
-    ''' 
+    '''
     Calculates the chi-square value of the no-signal hypothesis (i.e background
-    only) for the passed values. Need an expectation - use the analyic form, 
-    using the hard coded scale of the exp. That depends on the binning, so pass 
+    only) for the passed values. Need an expectation - use the analyic form,
+    using the hard coded scale of the exp. That depends on the binning, so pass
     in as argument. The mass range must also be set - otherwise, its ignored.
     '''
     bin_heights, bin_edges = np.histogram(vals, range = mass_range, bins = nbins)
@@ -47,19 +47,19 @@ def get_B_chi(vals, mass_range, nbins, A, lamb):
     ys_expected = get_B_expectation(bin_edges + half_bin_width, A, lamb)
     chi = 0
 
-    # Loop over bins - all of them for now. 
+    # Loop over bins - all of them for now.
     for i in range( len(bin_heights) ):
         chi_nominator = (bin_heights[i] - ys_expected[i])**2
         chi_denominator = ys_expected[i]
         chi += chi_nominator / chi_denominator
-    
+
     return chi/float(nbins-2) # B has 2 parameters.
 
 
 def get_B_expectation(xs, A, lamb):
-    ''' 
-    Return a set of expectation values for the background distribution for the 
-    passed in x values. 
+    '''
+    Return a set of expectation values for the background distribution for the
+    passed in x values.
     '''
     return [A*np.exp(-x/lamb) for x in xs]
 
@@ -73,5 +73,3 @@ def get_SB_expectation(xs, A, lamb, mu, sig, signal_amp):
     for x in xs:
         ys.append(A*np.exp(-x/lamb) + signal_gaus(x, mu, sig, signal_amp))
     return ys
-
-
